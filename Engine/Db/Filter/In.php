113d7e04a7189deca10dbb4e3c9efd0967da89c3
Artem Onyshchenko <temafey@gmail.com>
2014-12-01 11:39:11 +0200

    Refactored all database filters

--- Engine/Db/Filter/In.php
+++ Engine/Db/Filter/In.php
@@ -19,15 +19,43 @@ class In extends Standart
     CONST CRITERIA_NOTEQ = "NOTIN";
 
     /**
-     * Return filter value string
+     * Apply filter to query builder
      *
      * @param \Engine\Mvc\Model\Query\Builder $dataSource
      * @return string
      */
-    protected function getFilterStr(Builder $dataSource)
+    public function filterWhere(Builder $dataSource)
     {
+        $model = $dataSource->getModel();
+        if ($this->_column === self::COLUMN_ID) {
+            $expr = $model->getPrimary();
+            $alias = $dataSource->getAlias();
+        } elseif ($this->_column === self::COLUMN_NAME) {
+            $expr = $model->getNameExpr();
+            $alias = $dataSource->getAlias();
+        } else {
+            $expr = $this->_column;
+            $alias = $dataSource->getCorrelationName($this->_column);
+        }
+        if (!$alias) {
+            throw new \Engine\Exception("Field '".$this->_column."' not found in query builder");
+        }
         $compare = $this->getCompareCriteria();
-        
+        $this->setBoundParamKey($alias."_".$expr);
+
+        return $alias.".".$expr." ".$compare." (:".$this->getBoundParamKey().":)";
+    }
+
+    /**
+     * Return bound params array
+     *
+     * @param \Engine\Mvc\Model\Query\Builder $dataSource
+     * @return array
+     */
+    public function getBoundParams(Builder $dataSource)
+    {
+        $key = $this->getBoundParamKey();
+
         $values = $this->_value;
         if (!is_array($values)) {
             $values = [$values];
@@ -37,9 +65,9 @@ class In extends Standart
             $value = $adapter->escapeString($value);
         }
         if (count($values) == 0) {
-            return "";
+            return false;
         }
-        
-        return $compare."(".implode(", ", $values).")";
+
+        return [$key => $values];
     }
 }
